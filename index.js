fb7597943c481022dd5e7e7ca8ea5db740e3a65f
bamboo <bamboo@openresty.com>
2022-10-22 22:56:49 +0800

    stupid try.

--- index.js
+++ index.js
@@ -2,17 +2,18 @@
 process.env.NC_DB = `mysql2://${process.env.TIDB_HOST}:${process.env.TIDB_PORT}?u=${process.env.TIDB_USER}&p=${process.env.TIDB_PASSWORD}&d=nocodb`;
 
 const app = require('express')();
-const { Noco } = require('nocodb');
+// const { Noco } = require('nocodb');
+
+app.get('/api', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! World</a>`);
+});
 
 (async () => {
   try {
     const httpServer = app.listen(process.env.PORT || 8080);
-      app.get('/api', (req, res) => {
-        res.setHeader('Content-Type', 'text/html');
-      res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-      res.end(`Hello! World</a>`);
-    });
-    app.use(await Noco.init({}, httpServer, app));
+    // app.use(await Noco.init({}, httpServer, app));
     console.log(`Visit : localhost:${process.env.PORT}/dashboard`);
   } catch (e) {
     console.log(e);
