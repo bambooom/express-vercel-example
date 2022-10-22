347184d065e543a0c84dcd237fcd9a6a2f920ea9
bamboo <bamboo@openresty.com>
2022-10-22 23:03:28 +0800

    uncomment noco.

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
 process.env.NC_DB = `mysql2://${process.env.TIDB_HOST}:${process.env.TIDB_PORT}?u=${process.env.TIDB_USER}&p=${process.env.TIDB_PASSWORD}&d=nocodb`;
 
 const app = require('express')();
-// const { Noco } = require('nocodb');
+const { Noco } = require('nocodb');
 
 app.get('/api', (req, res) => {
   res.setHeader('Content-Type', 'text/html');
@@ -13,7 +13,7 @@ app.get('/api', (req, res) => {
 (async () => {
   try {
     const httpServer = app.listen(process.env.PORT || 8080);
-    // app.use(await Noco.init({}, httpServer, app));
+    app.use(await Noco.init({}, httpServer, app));
     console.log(`Visit : localhost:${process.env.PORT}/dashboard`);
   } catch (e) {
     console.log(e);
