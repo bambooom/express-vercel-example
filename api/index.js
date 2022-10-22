97baa1f03ddbaf507eb9b7b4f318324e17976cf8
bamboo <bamboo@openresty.com>
2022-10-22 23:14:44 +0800

    try inside async function.

--- api/index.js
+++ api/index.js
@@ -2,18 +2,17 @@
 process.env.NC_DB = `mysql2://${process.env.TIDB_HOST}:${process.env.TIDB_PORT}?u=${process.env.TIDB_USER}&p=${process.env.TIDB_PASSWORD}&d=nocodb`;
 
 const app = require('express')();
-const { Noco } = require('nocodb');
-
-app.get('/api', (req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! World</a>`);
-});
+// const { Noco } = require('nocodb');
 
 (async () => {
   try {
     const httpServer = app.listen(process.env.PORT || 8080);
-    app.use(await Noco.init({}, httpServer, app));
+    // app.use(await Noco.init({}, httpServer, app));
+    app.get('/api', (req, res) => {
+      res.setHeader('Content-Type', 'text/html');
+      res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+      res.end(`Hello! World</a>`);
+    });
     console.log(`Visit : localhost:${process.env.PORT}/dashboard`);
   } catch (e) {
     console.log(e);
